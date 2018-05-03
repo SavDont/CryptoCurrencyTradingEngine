ff7d343e3965e0a849f7f2566f97db0568604e3b
sd549 <31018667+cornellhedgefund@users.noreply.github.com>
2018-05-02 22:14:03 -0400

    backtest fully complete; sma strate implemented

--- strategy.py
+++ strategy.py
@@ -1,14 +1,17 @@
 from poloniex import Poloniex
-from IchimokuCloud import IchimokuClouds
+from IchimokuClouds import IchimokuClouds
+from SMAMomentum import SMAMomentum
 import time
 import datetime
+import pandas as pd
 #Constants below
 
 LONG_TRADE = 0
 SHORT_TRADE = 1
+NO_TRADE = -1
 
 class Trade:
-    def __init__(self, ticker, timestamp, price, amount)
+    def __init__(self, ticker, timestamp, price, amount):
         self.ticker = ticker
         self.timestamp = timestamp
         self.price = price
@@ -25,13 +28,13 @@ class Strategy:
         self.polo.key = 'KEFPS6QC-CE8KHULI-ACLJY1GC-L4P923CC'
         self.polo.secret = '4121c780577b601b051c2de8325e977cab65dbee250027c166d52762d7bee3dbd48245d9e0c89830a327fd05f0481c0433556be2c8a9bc6d775e8a84c6ad7df361'
 
-    def calculate_shares_on_hand(order_book):
-        total = 0
+    def calculate_shares_on_hand(self, order_book):
+        total = 0.0
         for trade in order_book:
             total += trade.amount
         return total
 
-    def get_data(self, start_date, end_date, ticker):
+    def get_data(self, start_date, end_date, period,  ticker):
         '''
         get_data(self, start_date, end_date, ticker) - gets the historical data
         as a dataframe
@@ -40,14 +43,13 @@ class Strategy:
             currency pair to trade on.
             Returns: a python data frame where each row represents a ticker
         '''
-        start_unix = time.mktime(datetime.datetime.strptime(start_date, "%d/%m/%Y").timetuple())
+        start_unix = time.mktime(datetime.datetime.strptime(start_date, "%m/%d/%Y").timetuple())
         end_unix = time.mktime(datetime.datetime.strptime(end_date, "%m/%d/%Y").timetuple())
-        historical_data = polo.returnChartData(currencyPair=ticker, period=period, start=start_unix,
+        historical_data = self.polo.returnChartData(currencyPair=ticker, period=period, start=start_unix,
                                                end=end_unix)
         data_frame = pd.DataFrame.from_dict(historical_data, orient='columns',
                                             dtype=None)
-        data_frame = data_frame[['date', 'high', 'low', 'open', 'close', 'quoteVolume', 'volume',
-                'weightedAverage']]
+
         return data_frame
 
 
@@ -66,21 +68,25 @@ class Strategy:
             Returns: A dictionary representing performance metrics of the 
             backtest including Profit/Loss and volatility.
         '''
-        security_df = get_data(start_date, end_date, ticker)
+        security_df = self.get_data(start_date, end_date, 300, ticker)
         order_book = []
         liquid_funds =  initial_deposit
         for index, row in security_df.iterrows():
+            #at each iteration send a row from dataframe into the algo
             trade = self.algo(order_book, ticker, liquid_funds, row)
+            datestring = time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(int(row['date'])))
 
             if trade['order'] == LONG_TRADE:
-                if row['weightedAverage'] * trade['shares'] > liquid_funds:
+                print("Long trade: " + str(trade['shares']) + " shares @ " + row['weightedAverage'] + " on " + datestring)
+                if float(row['weightedAverage']) * trade['shares'] > liquid_funds:
                     raise ValueError('Trade size exceeds liquid funds')
                 else:
-                    new_trade = Trade(ticker, index, row['weightedAverage'], trade['shares'])
+                    new_trade = Trade(ticker, index, float(row['weightedAverage']), trade['shares'])
                     order_book.append(new_trade)
-                    liquid_funds -= row['weightedAverage'] * trade['shares']
+                    liquid_funds -= float(row['weightedAverage']) * trade['shares']
             elif trade['order'] == SHORT_TRADE:
-                shares_oh = calculate_shares_on_hand(order_book)
+                print("Short trade: " + str(trade['shares']) + " shares @ " + row['weightedAverage'] + " on " + datestring)
+                shares_oh = self.calculate_shares_on_hand(order_book)
                 if trade['shares'] > shares_oh:
                     raise ValueError('Trade size exceeds shares in portfolio')
                 else:
@@ -95,15 +101,33 @@ class Strategy:
                                 new_order_book.append(Trade(value.ticker, value.timestamp, value.price, new_amt))
 
                             shares_to_sell = 0
-                            liquid_funds += row['weightedAverage'] * shares_to_sell
+                            liquid_funds += float(row['weightedAverage']) * shares_to_sell
 
                         else:
                             shares_to_sell -= value.amount
-                            liquid_funds += row['weightedAverage'] * value.amount
+                            liquid_funds += float(row['weightedAverage']) * value.amount
+                    order_book = new_order_book
+            last_row = row
+
+        if len(order_book) > 0:
+            #closes last open trades
+            for counter, value in enumerate(order_book):
+                liquid_funds += float(last_row['weightedAverage']) * value.amount
+
                             
         return liquid_funds 
         
 if __name__ == "__main__":
+    sma_strat = SMAMomentum()
+    start_date = "03/01/2018"
+    end_date = "05/01/2018"
+    ticker = "BTC_ETH"
+    initial_deposit = 5000
+    fees = 0.1
+    print ("********Simulating from " + start_date + " to " + end_date)
+    strat = Strategy(sma_strat.algo, ticker)
+    print ("********Total Returns: " + str(strat.backtest(start_date, end_date, ticker, initial_deposit, fees)))
+    '''
     ichimoku_strat = IchimokuClouds(9, 26, 52)
     start_date = "06/01/2017"
     end_date = "04/01/2018"
@@ -112,3 +136,4 @@ if __name__ == "__main__":
     fees = 0.1
     strat = Strategy(ichimoku_strat.cloud_algo, ticker)
     strat.backtest(self, start_date, end_date, ticker, initial_deposit, fees)
+    '''
